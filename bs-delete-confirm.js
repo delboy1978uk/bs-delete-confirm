26bd366ffb3c23178dfd85affc27ba87cb431bde
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-12-09 22:01:18 +0100

    upgrade to bootstrap 4

--- bs-delete-confirm.js
+++ bs-delete-confirm.js
@@ -3,18 +3,18 @@
         deleteConfirm: function(options)
         {
             var defaults =
-            {
-                heading: "Please confirm",
-                body: 'Are you sure you wish to perform this action?',
-                ok_text: 'Proceed',
-                cancel_text: 'Back',
-                class: 'primary',
-                log: false
-            };
+                {
+                    heading: "Please confirm",
+                    body: 'Are you sure you wish to perform this action?',
+                    ok_text: 'Proceed',
+                    cancel_text: 'Back',
+                    class: 'primary',
+                    log: false
+                };
 
             options = $.extend(defaults,options);
 
-            $('body').append('<div class="modal fade" id="bs-delete-confirm" tabindex="-1" role="dialog" aria-labelledby="bs-delete-confirm-label" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="bs-delete-confirm-label">Please confirm</h4></div><div id="bs-delete-confirm-body" class="modal-body"></div><div class="modal-footer"><a id="bs-delete-confirm-cancel" class="btn btn-default" data-dismiss="modal" aria-hidden="true"></a><a id="bs-delete-confirm-ok" class="btn btn-' + options.class + '"></a></div></div></div></div>');
+            $('body').append('<div tabindex="-1" class="modal fade" id="bs-delete-confirm" tabindex="-1" role="dialog" aria-labelledby="bs-delete-confirm-label" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="bs-delete-confirm-label">Please confirm</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div id="bs-delete-confirm-body" class="modal-body"></div><div class="modal-footer"><a id="bs-delete-confirm-cancel" class="btn btn-default" data-bs-dismiss="modal" aria-hidden="true"></a><a id="bs-delete-confirm-ok" class="btn btn-' + options.class + '"></a></div></div></div></div>');
 
             $('#bs-delete-confirm-label').html(options.heading);
             $('#bs-delete-confirm-body').html(options.body);
@@ -23,15 +23,16 @@
 
             this.each(function() {
                 $(this).on('click',function(e){
-                  e.preventDefault();
-                  link = $(this).prop('href');
-                  $('#bs-delete-confirm-ok').prop('href',link);
-                  $('#bs-delete-confirm').modal();
-                  if(options.log == true){
-                      console.log('Clicked link = ' + link);
-                  }
+                    e.preventDefault();
+                    link = $(this).prop('href');
+                    $('#bs-delete-confirm-ok').prop('href',link);
+                    $('#bs-delete-confirm').modal();
+                    if(options.log == true){
+                        console.log('Clicked link = ' + link);
+                    }
                 });
             });
         }
     });
 })(jQuery);
+
